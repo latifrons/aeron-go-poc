ceb395ab00cf70a7b93c337ea95642bd61021f70
Ryan Gan <ryan.gan@hkbitex.com.hk>
2025-07-18 15:57:11 +0800

    fix java args

--- driver/src/main/java/latifrons/Launcher.java
+++ driver/src/main/java/latifrons/Launcher.java
@@ -2,14 +2,17 @@ package latifrons;
 
 public class Launcher {
     public static void main(String[] args) {
-        if (args.length < 2) {
+        for (String arg : args) {
+            System.out.println("Arg: " + arg);
+        }
+        if (args.length < 1) {
             System.out.println("Usage: java latifrons.Launcher <command> [<args>]");
             System.out.println("Available commands:");
             System.out.println("  solo - Start the solo server");
             System.out.println("  cluster - Start the cluster server");
             return;
         }
-        String command = args[1];
+        String command = args[0];
 
         switch (command) {
             case "solo":
